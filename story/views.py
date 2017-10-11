4625d62e2b13c9602e64c2b266f7d051d39949da
root <root@kart-tree.ru>
2017-10-12 01:35:15 +0300

    aaa

--- story/views.py
+++ story/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from .models import Product, Provider
+from story.models import Product, Provider
 import numpy as np
 import pandas as pd
 import random
@@ -16,7 +16,10 @@ def allproducts(request):
 def product(request,id):
     product = Product.objects.get(pk=id)
     #x.apply(lambda row: "[new Date('" + str(row.Date) + "'), " + str(row.Close),axis=1) + "]"
+    #product = Product.objects.all()[0]
     x = pd.read_csv('story/static/' + product.tspath)
-    data = np.array(x.apply(lambda row: str(['new Date('+str(row.Date).replace('-',',')+')', row.Close]),axis=1))
+    x.Date = pd.to_datetime(x.Date)
+    x = pd.merge(x,x.set_index(pd.DatetimeIndex(x.Date)).groupby([pd.TimeGrouper('M')]).agg({'Date':'max'}),'inner',['Date'])
+    data = np.array(x.apply(lambda row: str(['new Date('+str(row.Date.date()).replace('-',',')+')', row.Close]),axis=1))
 #    data = np.array(x.apply(lambda row: [int(x) for x in str(row.Date).split('-')] + [row.Close],axis=1))[1]
     return render_to_response("story/" + product.templatepath, {'product': product, 'data': data})
